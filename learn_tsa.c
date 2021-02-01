d7735ed9479c9d752fafd19d976beee4fd7af406
nick <whatever@email.com>
2021-02-01 18:01:05 +0800

    not very correct... the way i annotate

--- learn_tsa.c
+++ learn_tsa.c
@@ -1,6 +1,30 @@
 #include "learn_tsa.h"
 #include <stdio.h>
 
+struct MyMutex;
+int resource = 0;
+
+
+void Lock()
+{
+    printf("Lock()\n");
+}
+
+void Unlock()
+{
+    printf("Unlock()\n");
+}
+
+
+void locklock()
+{
+    Lock();
+    resource = 1;
+    printf("resource = %d", resource);
+    Unlock();
+}
+
+
 int main()
 {
     printf("Asdsadasdasdsa\n");
