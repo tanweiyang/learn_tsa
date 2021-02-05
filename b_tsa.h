20f9454d8667c38e6ce5988d5d19d8d49b9a3618
nick <whatever@email.com>
2021-02-05 18:12:53 +0800

    added b_tsa

--- b_tsa.h
+++ b_tsa.h
@@ -3,5 +3,34 @@
 
 #include "clang_tsa.h"
 
+#define TSA_DECLARE_STRUCT_CAPABILITY(structname) \
+    struct CAPABILITY(#structname) structname
+
+#define TSA_DECLARE_MUTEX(mutexname) \
+    extern struct mutex mutexname
+
+#define TSA_DECLARE_SPINLOCK(lockname) \
+    extern spinlock_t lockname
+
+#define TSA_DECLARE_HLIST_HEAD(name) \
+    extern struct hlist_head name
+
+
+TSA_DECLARE_STRUCT_CAPABILITY(mutex);
+TSA_DECLARE_STRUCT_CAPABILITY(spinlock);
+
+TSA_DECLARE_HLIST_HEAD(binder_deferred_list);
+TSA_DECLARE_MUTEX(binder_deferred_lock);
+
+TSA_DECLARE_HLIST_HEAD(binder_devices);
+TSA_DECLARE_HLIST_HEAD(binder_procs);
+TSA_DECLARE_MUTEX(binder_procs_lock);
+
+TSA_DECLARE_HLIST_HEAD(binder_dead_nodes);
+TSA_DECLARE_SPINLOCK(binder_dead_nodes_lock);
+
+extern atomic_t binder_last_id;
+
+
 #endif // B_TSA_H
 
